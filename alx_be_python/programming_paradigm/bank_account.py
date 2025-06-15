e19698657a3e35ba37ba3dc428eb4a3d412f8e76
Aadam Longi <aadamdimah@gmail.com>
2025-06-15 15:15:10 +0000

    Completed Task 0: BankAccount class and command-line interface

--- alx_be_python/programming_paradigm/bank_account.py
+++ alx_be_python/programming_paradigm/bank_account.py
@@ -1,22 +1,17 @@
 class BankAccount:
-    def __init__(self, balance=0):
-        self.account_balance = balance
+    def __init__(self, initial_balance=0):
+        self.account_balance = initial_balance
 
     def deposit(self, amount):
-        if amount > 0:
-            self.account_balance += amount
-        else:
-            print("Deposit amount must be positive.")
+        self.account_balance += amount
 
     def withdraw(self, amount):
-        if amount > self.account_balance:
-            return False
-        elif amount <= 0:
-            print("Withdrawal amount must be positive.")
-            return False
-        else:
+        if amount <= self.account_balance:
             self.account_balance -= amount
             return True
+        else:
+            return False
 
     def display_balance(self):
         print(f"Current Balance: ${self.account_balance}")
+
