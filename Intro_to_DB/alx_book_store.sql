6f077c926b2d03c78c32b88d74fdef56a6a9d843
Aadam Longi <aadamdimah@gmail.com>
2025-07-07 00:35:25 +0000

    Task 0: Created schema for alx_book_store with all required tables

--- Intro_to_DB/alx_book_store.sql
+++ Intro_to_DB/alx_book_store.sql
@@ -1,48 +1,46 @@
 -- Create the database
 CREATE DATABASE IF NOT EXISTS alx_book_store;
-
--- Use the database
 USE alx_book_store;
 
--- Create Authors table
-CREATE TABLE IF NOT EXISTS Authors (
+-- Create the Authors table
+CREATE TABLE IF NOT EXISTS authors (
     author_id INT PRIMARY KEY,
     author_name VARCHAR(215)
 );
 
--- Create Books table
-CREATE TABLE IF NOT EXISTS Books (
+-- Create the Books table
+CREATE TABLE IF NOT EXISTS books (
     book_id INT PRIMARY KEY,
     title VARCHAR(130),
     author_id INT,
     price DOUBLE,
     publication_date DATE,
-    FOREIGN KEY (author_id) REFERENCES Authors(author_id)
+    FOREIGN KEY (author_id) REFERENCES authors(author_id)
 );
 
--- Create Customers table
-CREATE TABLE IF NOT EXISTS Customers (
+-- Create the Customers table
+CREATE TABLE IF NOT EXISTS customers (
     customer_id INT PRIMARY KEY,
     customer_name VARCHAR(215),
     email VARCHAR(215),
     address TEXT
 );
 
--- Create Orders table
-CREATE TABLE IF NOT EXISTS Orders (
+-- Create the Orders table
+CREATE TABLE IF NOT EXISTS orders (
     order_id INT PRIMARY KEY,
     customer_id INT,
     order_date DATE,
-    FOREIGN KEY (customer_id) REFERENCES Customers(customer_id)
+    FOREIGN KEY (customer_id) REFERENCES customers(customer_id)
 );
 
--- Create Order_Details table
-CREATE TABLE IF NOT EXISTS Order_Details (
+-- Create the Order_Details table
+CREATE TABLE IF NOT EXISTS order_details (
     orderdetailid INT PRIMARY KEY,
     order_id INT,
     book_id INT,
     quantity DOUBLE,
-    FOREIGN KEY (order_id) REFERENCES Orders(order_id),
-    FOREIGN KEY (book_id) REFERENCES Books(book_id)
+    FOREIGN KEY (order_id) REFERENCES orders(order_id),
+    FOREIGN KEY (book_id) REFERENCES books(book_id)
 );
 
